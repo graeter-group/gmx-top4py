ac76c6b3199d717833889ea92248daff7f50962f
emerbitz <78315455+emerbitz@users.noreply.github.com>
2025-10-02 14:49:54 +0200

    add install option via requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    packages=find_packages(where="src"),
-    name='gmx-top4py',
-    version='0.0.0',
-    include_package_data=True,
-)
+setup()
