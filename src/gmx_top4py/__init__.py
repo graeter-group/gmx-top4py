f65c9136afeb42a06606a1184a3edfb86510a222
emerbitz <78315455+emerbitz@users.noreply.github.com>
2025-11-03 16:41:58 +0100

    remove deprecation warning

--- src/gmx_top4py/__init__.py
+++ src/gmx_top4py/__init__.py
@@ -4,4 +4,8 @@ Welcome to gmx-top4py!
 
 from __future__ import annotations
 import warnings
-warnings.warn("gmx-top4py has been renamed to gmxtop. Use 'pip install gmxtop'.", DeprecationWarning)
+
+warnings.warn(
+    "gmx-top4py has been renamed to gmxtop. Use 'pip install gmxtop'.",
+    DeprecationWarning,
+)
