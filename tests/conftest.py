03a142309f5c3790a6eb3a4916045e517f4b8353
emerbitz <78315455+emerbitz@users.noreply.github.com>
2025-11-03 14:04:17 +0100

    add attribution header

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,27 @@
+# This file is part of the gmx-top4py project.
+#
+# The gmx-top4py project is based on or includes code from:
+#    kimmdy (https://github.com/graeter-group/kimmdy/tree/main)
+#    Copyright (C) graeter-group
+#    Licensed under the GNU General Public License v3.0 (GPLv3).
+#
+# Modifications and additional code:
+#    Copyright (C) 2025 graeter-group
+#    Licensed under the GNU General Public License v3.0 (GPLv3).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 """File for pytest configuration in python and fixture definition.
 
 The name 'conftest.py' is recognized by pytest to execute it before tests.
